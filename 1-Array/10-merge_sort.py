b3d6c419c5d5d0ada7894fa50c938941fc02f6cf
Sadam-Barkat <sadambarkat405@gmail.com>
2025-04-16 09:39:04 +0500

    Add Files

--- 1-Array/10-merge_sort.py
+++ 1-Array/10-merge_sort.py
@@ -1,93 +1,56 @@
-#include<iostream>
-using namespace std;
+def merge(arr, left, mid, right):
+    n1 = mid - left + 1
+    n2 = right - mid
 
-void Merge(int arr[], int left, int mid, int right)
-{
-	int n1=(mid-left)+1;
-	int n2=right-mid;
-	
-	int L[n1], R[n2];
-	
-	//put values for left half array
-	for(int i=0; i<n1; i++)
-	{
-		L[i]=arr[left+i];
-	}
-	
-	//put values for right half array
-	for(int j=0; j<n2; j++)
-	{
-		R[j]=arr[mid+1+j];
-	}
-	
-	//Mergign L[n1] and R[n2]
-	int i=0, j=0, k=left; 
-	while(i<n1 && j<n2)
-	{
-		if(L[i]<=R[j])
-		{
-			arr[k]=L[i];
-			i++;
-			k++;
-		}
-		else
-		{
-			arr[k]=R[j];
-			j++;
-			k++;
-		}
-	}
-	
-	while(i<n1)
-	{
-		arr[k]=L[i];
-		i++; 
-		k++;
-	}
-	
-	while(j<n2)
-	{
-		arr[k]=R[j];
-		j++;
-		k++;
-	}
-}
+    # Create temp arrays
+    L = [0] * n1
+    R = [0] * n2
 
-void Mergesort(int arr[],int left, int right)
-{
-	if(left<right)
-	{
-		int mid=left+(right-left)/2;
-		
-		//recursive call for left half
-		Mergesort(arr, left, mid);
-		
-		//recursive call for right half
-		Mergesort(arr, mid+1, right);
-		
-		//call for merging two sorted arrays
-		Merge(arr, left, mid, right);
-	}
-}
+    # Copy data to temp arrays L[] and R[]
+    for i in range(n1):
+        L[i] = arr[left + i]
 
-int main(){
-	int arr[]={6,3,9,5,2,8,7,1};
-	int size =sizeof(arr)/sizeof(0);
-	
-	cout<<"Array Before Sorting"<<endl;
-	for(int i=0; i<size; i++)
-	{
-		cout<<arr[i]<<" ";
-	}
-	cout<<endl;
-	
-    Mergesort(arr,0,size-1);
-    cout<<"Array After Sorting"<<endl;
-    for(int i=0; i<size; i++)
-    {
-    	cout<<arr[i]<<" ";
-	}
-	cout<<endl;
-	
-	return 0;
-}
+    for j in range(n2):
+        R[j] = arr[mid + 1 + j]
+
+    # Merge the temp arrays back into arr[left..right]
+    i = j = 0
+    k = left
+
+    while i < n1 and j < n2:
+        if L[i] <= R[j]:
+            arr[k] = L[i]
+            i += 1
+        else:
+            arr[k] = R[j]
+            j += 1
+        k += 1
+
+    # Copy any remaining elements of L[]
+    while i < n1:
+        arr[k] = L[i]
+        i += 1
+        k += 1
+
+    # Copy any remaining elements of R[]
+    while j < n2:
+        arr[k] = R[j]
+        j += 1
+        k += 1
+
+def merge_sort(arr, left, right):
+    if left < right:
+        mid = left + (right - left) // 2
+        merge_sort(arr, left, mid)
+        merge_sort(arr, mid + 1, right)
+        merge(arr, left, mid, right)
+
+# Driver code
+arr = [6, 3, 9, 5, 2, 8, 7, 1]
+print("Array Before Sorting")
+print(arr)
+
+merge_sort(arr, 0, len(arr) - 1)
+
+print("Array After Sorting")
+print(arr)
