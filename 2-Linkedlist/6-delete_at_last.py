51e70b5b92dadbef1a38d14fed552579fa601acf
Sadam-Barkat <sadambarkat405@gmail.com>
2025-04-27 23:06:41 +0500

    Add Files

---
+++ 2-Linkedlist/6-delete_at_last.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+def delete_at_last(node):
+    head = node
+    current = head
+    while current.next.next is not None:
+        current = current.next
+    current.next = None
+    return head    
+
+def display(node):
+    head = node
+    while head is not None:
+        print(head.data)
+        head = head.next
+
+node1 = Node(10)
+node2 = Node(20)
+node3 = Node(30)
+node4 = Node(40)
+node5 = Node(50)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+
+display(delete_at_last(node1))
+
+
